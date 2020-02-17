6e5fc868ef0bed4033e520dad9b71492d73bad2f
Wendy Harris <wendyv510@hotmail.com>
2020-02-17 18:35:37 +0000

    Automatically backed up by Learn

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,47 @@
 class TweetsController < ApplicationController
-
-
+  
+  get '/tweets' do 
+     @tweeets = Tweet.all 
+     
+     erb :index 
+   end 
+   
+   get '/tweets/new' do
+     erb :new 
+   end 
+   
+   post '/tweets' do 
+     @tweet = Tweet.create(content: params[:content]) 
+     @tweet.save 
+     
+      redirect to "/tweets/#{@tweet.id}"  
+   end 
+   
+   get '/tweets/:id' do 
+     @tweet = Tweet.find_by_id(params[:id]) 
+     
+     erb :show 
+   end
+   
+   get '/tweets/:id/edit' do 
+     @tweet = Tweet.find_by_id(params[:id]) 
+    
+    erb :show 
+   end 
+  
+   patch '/tweets/:id' do 
+     @tweet = Tweet.find_by_id(params[:id]) 
+     @tweet.content = params[:content] 
+     
+     redirect to "/tweets/#{@tweet.id}" 
+   end 
+   
+   delete '/tweets/:id' do 
+     @tweet = Tweet.find_by_id(params[:id])
+     @tweet.delete 
+     
+     redirect to "'/tweets" 
+   end 
+   
+      
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,43 @@
 class UsersController < ApplicationController
-
+  
+  get '/users/signup' do 
+    
+    erb :signup 
+  end 
+  
+  post '/signup' do 
+    @user = User.new(username: params[:username], email: params[:email], password: params[:password]) 
+    @user.save 
+    session[:user_id] = user.id 
+    
+    redirect to "/users/home" 
+  end 
+  
+  get '/login' do 
+    
+    erb :login 
+  end 
+  
+  post '/login' do 
+    @user = User.find_by(email: params[:email], password: params[:password]) 
+       if @user 
+         session[:user_id] = user.id 
+           redirect to '/users/home' 
+       else 
+          redirect to '/login' 
+       end 
+  end 
+  
+  get '/logout' do 
+    session.clear 
+      redirect to '/' 
+  end 
+  
+  get '/users/home' do 
+    @user = User.find(session[:user.id])
+    
+    erb :home 
+  end 
+  
 
 end
